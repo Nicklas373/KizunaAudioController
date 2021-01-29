e588521f6e39b905b38fd430100562a73ea0270f
Dicky Herlambang (花） <41816010078@student.mercubuana.ac.id>
2021-01-30 01:34:48 +0700

    AudioNode: Include hph audio kernel path
    Signed-off-by: Dicky Herlambang (花） <41816010078@student.mercubuana.ac.id>

--- app/src/main/java/com/hana/kizunaaudiocontroller/AudioNode.java
+++ app/src/main/java/com/hana/kizunaaudiocontroller/AudioNode.java
@@ -5,8 +5,11 @@ public class AudioNode {
     // Kernel audio path
     public String uhqa_kernel_3_x = "/sys/module/snd_soc_wcd9335/parameters/huwifi_mode";
     public String uhqa_kernel_4_x = "/sys/module/wcd9335_dlkm/parameters/huwifi_mode";
+    public String hph_kernel_3_x = "/sys/module/snd_soc_wcd9330/parameters/high_perf_mode";
     public String uhqa_file = "uhqa.txt";
+    public String hph_file = "hph.txt";
     public String kernel_file = "kdump.txt";
     public String uhqa_force_file = "/sdcard/Android/data/com.hana.kizunaaudiocontroller/files/Download/uhqa.txt";
+    public String hph_force_file = "/sdcard/Android/data/com.hana.kizunaaudiocontroller/files/Download/hph.txt";
     public String kernel_dump = "/sdcard/Android/data/com.hana.kizunaaudiocontroller/files/Download/kdump.txt";
 }
