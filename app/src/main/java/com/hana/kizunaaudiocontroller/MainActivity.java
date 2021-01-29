03c108f2e7f208efb1785b8c0c1fc129c3ceb2b4
Dicky Herlambang (花） <41816010078@student.mercubuana.ac.id>
2021-01-30 01:42:57 +0700

    MainActivity: Misc changes
    * Implement theme switcher from AudioConfActivity
    * Replace all placeholde with current nodes
    * Set transition animation as required from AudioConfActivity
    
    Signed-off-by: Dicky Herlambang (花） <41816010078@student.mercubuana.ac.id>

--- app/src/main/java/com/hana/kizunaaudiocontroller/MainActivity.java
+++ app/src/main/java/com/hana/kizunaaudiocontroller/MainActivity.java
@@ -1,22 +1,29 @@
 package com.hana.kizunaaudiocontroller;
 
+import android.Manifest;
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.transition.Explode;
+import android.transition.Fade;
+import android.view.View;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.cardview.widget.CardView;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-import android.Manifest;
-import android.app.ActivityOptions;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
+import com.google.android.material.switchmaterial.SwitchMaterial;
 
 public class MainActivity extends AppCompatActivity {
 
     // Declare controller
-    CardView placeholder_1;
+    CardView mm_1;
+    SwitchMaterial theme_switcher;
 
     // Declare variables
     private static final int REQUEST_PERMISSIONS = 20;
@@ -27,23 +34,54 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Bind controller
-        placeholder_1 = findViewById(R.id.placeholder_1);
+        mm_1 = findViewById(R.id.cv_app_main_menu_1);
+        theme_switcher = findViewById(R.id.theme_switcher);
+
+        // Binding root class
+        AudioRoot ar = new AudioRoot();
 
         // Ask necessary permission for storage access
-        ask_perm();
+        ar.run(this::ask_perm);
 
         // Ask necessary root access
-        AudioRoot ar = new AudioRoot();
         ar.run(ar::checkRooted);
 
-        placeholder_1.setOnClickListener(v -> {
+        // Sharedprefences begin
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("KAO_MAIN_PREF", 0);
+        SharedPreferences.Editor save = pref.edit();
+
+        // Getting sharedpreferences value if exist
+        boolean night_mode = pref.getBoolean("MODE_NIGHT", false);
+        if (night_mode) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
+
+        // Set an animation transition
+        getWindow().setEnterTransition(new Explode());
+        getWindow().setReturnTransition(new Fade());
+
+        mm_1.setOnClickListener(v -> {
             Intent i = new Intent(MainActivity.this, AudioConfActivity.class);
 
-            View sharedView = placeholder_1;
+            View sharedView = mm_1;
             String transitionName = getString(R.string.PLACEHOLDER);
 
             ActivityOptions transitionActivityOptions = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this, sharedView, transitionName);
             startActivity(i, transitionActivityOptions.toBundle());
+        });
+
+        theme_switcher.setOnClickListener(v -> {
+            if (theme_switcher.isChecked()) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                save.putBoolean("MODE_NIGHT", false);
+                save.apply();
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                save.putBoolean("MODE_NIGHT", true);
+                save.apply();
+            }
         });
     }
 
