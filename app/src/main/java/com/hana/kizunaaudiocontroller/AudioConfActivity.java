65fdc19f8f8247901e5fee0d7a9805518ab40859
Dicky Herlambang (花） <41816010078@student.mercubuana.ac.id>
2021-02-02 00:08:45 +0700

    AudioConfActivity: Introduce low distortion amp & headphone impedance detection switcher
    * Init configuration for low distort amp & impedance detection for compatible kernel
    * Switch to relativelayout (Will test with scrollview after this)
    
    Signed-off-by: Dicky Herlambang (花） <41816010078@student.mercubuana.ac.id>

--- app/src/main/java/com/hana/kizunaaudiocontroller/AudioConfActivity.java
+++ app/src/main/java/com/hana/kizunaaudiocontroller/AudioConfActivity.java
@@ -30,11 +30,11 @@ public class AudioConfActivity extends AppCompatActivity {
 
     // Declaring controller
     ConstraintLayout this_activity;
-    CardView cv_title, cv_uhqa, cv_hph;
-    SwitchCompat switch_uhqa, switch_hph;
-    Button button_uhqa, button_hph;
+    CardView cv_title, cv_uhqa, cv_hph, cv_amp, cv_impedance;
+    SwitchCompat switch_uhqa, switch_hph, switch_amp, switch_impedance;
+    Button button_uhqa, button_hph, button_amp, button_impedance;
     Dialog po;
-    TextView title, desc, desc_ext, uhqa_stats, hph_stats;
+    TextView title, desc, desc_ext, uhqa_stats, hph_stats, amp_stats, impedance_stats;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,12 +46,20 @@ public class AudioConfActivity extends AppCompatActivity {
         cv_title = findViewById(R.id.cv_app_menu_1);
         cv_uhqa = findViewById(R.id.cv_app_menu_1_1);
         cv_hph = findViewById(R.id.cv_app_menu_1_2);
+        cv_amp = findViewById(R.id.cv_app_menu_1_3);
+        cv_impedance = findViewById(R.id.cv_app_menu_1_4);
         uhqa_stats = findViewById(R.id.switch_app_menu_1_1_text);
         hph_stats = findViewById(R.id.switch_app_menu_1_2_1_text);
+        amp_stats = findViewById(R.id.switch_app_menu_1_3_1_text);
+        impedance_stats = findViewById(R.id.switch_app_menu_1_4_1_text);
         switch_uhqa = findViewById(R.id.switch_app_menu_1);
         switch_hph = findViewById(R.id.switch_app_menu_1_2);
+        switch_amp = findViewById(R.id.switch_app_menu_1_3);
+        switch_impedance = findViewById(R.id.switch_app_menu_1_4);
         button_uhqa = findViewById(R.id.button_app_menu_1);
         button_hph = findViewById(R.id.button_app_menu_1_2);
+        button_amp = findViewById(R.id.button_app_menu_1_3);
+        button_impedance = findViewById(R.id.button_app_menu_1_4);
 
         // Hide title bar
         Objects.requireNonNull(getSupportActionBar()).hide();
@@ -75,21 +83,29 @@ public class AudioConfActivity extends AppCompatActivity {
         // Check kernel version
         if (kernel_ver.compareTo(upstream) > 0) {
             au.DumpFile(an.uhqa_kernel_4_x, an.uhqa_force_file);
+            au.DumpFile(an.amp_kernel_4_x, an.amp_force_file);
 
             // Not supported on 4.9 Kernel since techpack aren't
             // use older than wcd9335
             au.WriteToFile("0", an.hph_force_file);
+            au.WriteToFile("0", an.impedance_force_file);
         } else if (kernel_ver.compareTo(legacy) > 0) {
             au.DumpFile(an.uhqa_kernel_3_x, an.uhqa_force_file);
             au.DumpFile(an.hph_kernel_3_x, an.hph_force_file);
+            au.DumpFile(an.amp_kernel_3_x, an.amp_force_file);
+            au.DumpFile(an.impedance_kernel_3_x, an.impedance_file);
         }
 
         // Set switch value on init
         String uhqa_value = readFromFile(this, an.uhqa_file);
         String hph_value = readFromFile(this, an.hph_file);
-        
+        String amp_value = readFromFile(this, an.amp_file);
+        String impedance_value = readFromFile(this, an.impedance_file);
+
         switch_uhqa.setChecked(uhqa_value.compareTo("1") > 0);
         switch_hph.setChecked(hph_value.compareTo("1") > 0);
+        switch_amp.setChecked(amp_value.compareTo("1") > 0);
+        switch_impedance.setChecked(impedance_value.compareTo("1") > 0);
 
         cv_title.setOnClickListener(v -> {
             Intent i = new Intent(AudioConfActivity.this, MainActivity.class);
@@ -150,6 +166,58 @@ public class AudioConfActivity extends AppCompatActivity {
             }
         });
 
+        switch_amp.setOnClickListener(v -> {
+            if (kernel_ver.compareTo(upstream) > 0) {
+                if (switch_amp.isChecked()) {
+                    au.WriteToFile("1", an.amp_kernel_4_x);
+                    amp_stats.setText(String.format("%s %s | %s", getResources().getString(R.string.state_info), getResources().getString(R.string.state_support), getResources().getString(R.string.state_enable)));
+                    amp_stats.setVisibility(View.VISIBLE);
+                    au.DropFile(an.amp_file);
+                } else {
+                    au.WriteToFile("0", an.amp_kernel_4_x);
+                    amp_stats.setText(String.format("%s %s | %s", getResources().getString(R.string.state_info), getResources().getString(R.string.state_support), getResources().getString(R.string.state_disable)));
+                    amp_stats.setVisibility(View.VISIBLE);
+                    au.DropFile(an.amp_file);
+                }
+            } else if (kernel_ver.compareTo(legacy) > 0) {
+                if (switch_amp.isChecked()) {
+                    au.WriteToFile("1", an.amp_kernel_3_x);
+                    amp_stats.setText(String.format("%s %s | %s", getResources().getString(R.string.state_info), getResources().getString(R.string.state_support), getResources().getString(R.string.state_enable)));
+                    amp_stats.setVisibility(View.VISIBLE);
+                    au.DropFile(an.amp_file);
+                } else {
+                    au.WriteToFile("0", an.amp_kernel_3_x);
+                    amp_stats.setText(String.format("%s %s | %s", getResources().getString(R.string.state_info), getResources().getString(R.string.state_support), getResources().getString(R.string.state_disable)));
+                    amp_stats.setVisibility(View.VISIBLE);
+                    au.DropFile(an.amp_file);
+                }
+            }
+        });
+
+        switch_impedance.setOnClickListener(v -> {
+            if (kernel_ver.compareTo(upstream) > 0) {
+                switch_impedance.setChecked(false);
+                switch_impedance.setClickable(false);
+                switch_impedance.setEnabled(false);
+                setSnackBar(findViewById(android.R.id.content), "Your kernel doesn't support Headphone Impedance Detection Module");
+                impedance_stats.setText(String.format("%s %s | %s", getResources().getString(R.string.state_info), getResources().getString(R.string.state_unsupport), getResources().getString(R.string.state_disable)));
+                impedance_stats.setVisibility(View.VISIBLE);
+                au.DropFile(an.hph_file);
+            } else if (kernel_ver.compareTo(legacy) > 0) {
+                if (switch_impedance.isChecked()) {
+                    au.WriteToFile("1", an.impedance_kernel_3_x);
+                    impedance_stats.setText(String.format("%s %s | %s", getResources().getString(R.string.state_info), getResources().getString(R.string.state_support), getResources().getString(R.string.state_enable)));
+                    impedance_stats.setVisibility(View.VISIBLE);
+                    au.DropFile(an.impedance_file);
+                } else {
+                    au.WriteToFile("0", an.impedance_kernel_3_x);
+                    impedance_stats.setText(String.format("%s %s | %s", getResources().getString(R.string.state_info), getResources().getString(R.string.state_support), getResources().getString(R.string.state_disable)));
+                    impedance_stats.setVisibility(View.VISIBLE);
+                    au.DropFile(an.impedance_file);
+                }
+            }
+        });
+
         button_uhqa.setOnClickListener(v -> {
             // Initiate dialog
             po = new Dialog(AudioConfActivity.this);
@@ -164,6 +232,22 @@ public class AudioConfActivity extends AppCompatActivity {
 
             // Call dialog
             ShowPopup(button_hph,  getResources().getString(R.string.app_menu_2_text), getResources().getString(R.string.app_menu_2_desc), getResources().getString(R.string.app_menu_2_desc_ext));
+        });
+
+        button_amp.setOnClickListener(v -> {
+            // Initiate dialog
+            po = new Dialog(AudioConfActivity.this);
+
+            // Call dialog
+            ShowPopup(button_amp,  getResources().getString(R.string.app_menu_3_text), getResources().getString(R.string.app_menu_3_desc), getResources().getString(R.string.app_menu_3_desc_ext));
+        });
+
+        button_impedance.setOnClickListener(v -> {
+            // Initiate dialog
+            po = new Dialog(AudioConfActivity.this);
+
+            // Call dialog
+            ShowPopup(button_impedance,  getResources().getString(R.string.app_menu_4_text), getResources().getString(R.string.app_menu_4_desc), getResources().getString(R.string.app_menu_4_desc_ext));
         });
     }
 
