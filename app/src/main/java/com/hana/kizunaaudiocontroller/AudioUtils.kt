0d861270c16ef8c4c26a6268f508a629d0cd5024
Dicky Herlambang (花） <41816010078@student.mercubuana.ac.id>
2021-02-03 17:37:26 +0700

    AudioUtils: Don't use companion object for readFromFile
    So it can be call as alias on other class or kotlin file
    
    Signed-off-by: Dicky Herlambang (花） <41816010078@student.mercubuana.ac.id>

--- app/src/main/java/com/hana/kizunaaudiocontroller/AudioUtils.kt
+++ app/src/main/java/com/hana/kizunaaudiocontroller/AudioUtils.kt
@@ -7,6 +7,25 @@ import com.jaredrummler.android.shell.Shell
 import java.io.*
 
 class AudioUtils {
+    fun readFromFile(context: Context, filename: String?): String? {
+        var line: String? = null
+        try {
+            val fileInputStream = FileInputStream(File(context.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS), filename))
+            val inputStreamReader = InputStreamReader(fileInputStream)
+            val bufferedReader = BufferedReader(inputStreamReader)
+            val stringBuilder = StringBuilder()
+            while (bufferedReader.readLine().also { line = it } != null) {
+                stringBuilder.append(line + System.getProperty("line.separator"))
+            }
+            fileInputStream.close()
+            line = stringBuilder.toString()
+            bufferedReader.close()
+        } catch (fnfe: IOException) {
+            Log.e("Exception", "File not found $fnfe")
+        }
+        return line
+    }
+
     fun WriteToFile(value: String, data: String) {
         try {
             val write = Shell.SU.run("echo $value > $data")
@@ -36,27 +55,6 @@ class AudioUtils {
             val rkf = Shell.SU.run("uname -r | head -c 3 > $local_dir")
         } catch (e: Exception) {
             Log.e("Exception", "File drop failed: $e")
-        }
-    }
-
-    companion object {
-        fun readFromFile(context: Context, filename: String?): String? {
-            var line: String? = null
-            try {
-                val fileInputStream = FileInputStream(File(context.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS), filename))
-                val inputStreamReader = InputStreamReader(fileInputStream)
-                val bufferedReader = BufferedReader(inputStreamReader)
-                val stringBuilder = StringBuilder()
-                while (bufferedReader.readLine().also { line = it } != null) {
-                    stringBuilder.append(line + System.getProperty("line.separator"))
-                }
-                fileInputStream.close()
-                line = stringBuilder.toString()
-                bufferedReader.close()
-            } catch (fnfe: IOException) {
-                Log.e("Exception", "File not found $fnfe")
-            }
-            return line
         }
     }
 }
