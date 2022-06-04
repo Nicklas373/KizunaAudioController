5269d544ff8bcefb5ff06e24dad2e4cbf5722984
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2022-06-04 07:29:15 +0700

    AudioNode: Re-written to camelCase variable

--- app/src/main/java/com/hana/kizunaaudiocontroller/datasource/AudioNode.kt
+++ app/src/main/java/com/hana/kizunaaudiocontroller/datasource/AudioNode.kt
@@ -1,89 +1,119 @@
 package com.hana.kizunaaudiocontroller.datasource
 
+import android.annotation.SuppressLint
+
 class AudioNode {
     // Kernel audio path
-    var uhqa_kernel_3_x = "/sys/module/snd_soc_wcd9335/parameters/huwifi_mode"
-    var uhqa_kernel_4_x = "/sys/module/wcd9335_dlkm/parameters/huwifi_mode"
-    var hph_kernel_3_x = "/sys/module/snd_soc_wcd9330/parameters/high_perf_mode"
-    var amp_kernel_3_x = "/sys/module/snd_soc_wcd9335/parameters/low_distort_amp"
-    var amp_kernel_4_x = "/sys/module/wcd9335_dlkm/parameters/low_distort_amp"
-    var impedance_kernel_3_x = "/sys/module/snd_soc_wcd9xxx/parameters/impedance_detect_en"
-    var gating_kernel_3_x = "/sys/module/snd_soc_wcd9335/parameters/dig_core_collapse_enable"
-    var gating_kernel_4_x = "/sys/module/wcd9335_dlkm/parameters/dig_core_collapse_enable"
-    var uhqa_file = "uhqa.txt"
-    var hph_file = "hph.txt"
-    var amp_file = "amp.txt"
-    var impedance_file = "impedance.txt"
-    var gating_file = "gating.txt"
-    var kernel_file = "kdump.txt"
-    var audio_client_tl_init_file = "audio_client_tl_init.txt"
-    var audio_client_bl_init_file = "audio_client_bl_init.txt"
-    var audio_client_init_file = "audio_client_init.txt"
-    var library_files = "library.txt"
-    var kao_init_file = "kao_audio_init.txt"
-    var kao_top_limit_file = "kao_aclient_tl.txt"
-    var kao_bottom_limit_file = "kao_aclient_bl.txt"
-    var kao_io_init_file = "kao_io.txt"
-    var kao_standby_init_file = "kao_standby.txt"
-    var kao_rate_init_file = "kao_smp_rate.txt"
-    var kao_frm_cnt_init_file = "kao_frm_cnt.txt"
-    var kao_frm_bit_init_file = "kao_frm_bit.txt"
-    var kao_chn_cnt_init_file = "kao_chn_cnt.txt"
-    var kao_chn_mask_init_file = "kao_chn_mask.txt"
-    var kao_prc_bit_init_file = "kao_prc_bit.txt"
-    var kao_prc_frm_init_file = "kao_prc_frm.txt"
-    var kao_out_stream_init_file = "kao_out_stream.txt"
-    var kao_backup_dir = "/sdcard/Download"
-    var kao_backup_files = "KAO_BACKUP.gz"
-    var kao_files = "/sdcard/Android/data/com.hana.kizunaaudiocontroller/files/Download/"
-    var uhqa_force_file =
+    var uhqaKernelLegacy = "/sys/module/snd_soc_wcd9335/parameters/huwifi_mode"
+    var uhqaKernelUpstream = "/sys/module/wcd9335_dlkm/parameters/huwifi_mode"
+    var hphKernelLegacy = "/sys/module/snd_soc_wcd9330/parameters/high_perf_mode"
+    var ampKernelLegacy = "/sys/module/snd_soc_wcd9335/parameters/low_distort_amp"
+    var ampKernelUpstream = "/sys/module/wcd9335_dlkm/parameters/low_distort_amp"
+    var impedanceKernelLegacy = "/sys/module/snd_soc_wcd9xxx/parameters/impedance_detect_en"
+    var gatingKernelLegacy = "/sys/module/snd_soc_wcd9335/parameters/dig_core_collapse_enable"
+    var gatingKernelUpstream = "/sys/module/wcd9335_dlkm/parameters/dig_core_collapse_enable"
+    var uhqaFile = "uhqa.txt"
+    var hphFile = "hph.txt"
+    var ampFile = "amp.txt"
+    var impedanceFile = "impedance.txt"
+    var gatingFile = "gating.txt"
+    var kernelFile = "kdump.txt"
+    var audioClientTlInitFile = "audio_client_tlInit.txt"
+    var audioClientBlInitFile = "audio_client_blInit.txt"
+    var audioClientInitFile = "audio_clientInit.txt"
+    var libraryFiles = "library.txt"
+    var kaoInitFile = "kao_audioInit.txt"
+    var kaoTopLimitFile = "kao_aclient_tl.txt"
+    var kaoBottomLimitFile = "kao_aclient_bl.txt"
+    var kaoIoInitFile = "kao_io.txt"
+    var kaoStandbyInitFile = "kao_standby.txt"
+    var kaoRateInitFile = "kao_smp_rate.txt"
+    var kaoFrmCntInitFile = "kao_frm_cnt.txt"
+    var kaoFrmBitInitFile = "kao_frm_bit.txt"
+    var kaoChnCntInitFile = "kao_chn_cnt.txt"
+    var kaoChnMaskInitFile = "kao_chn_mask.txt"
+    var kaoPrcBitInitFile = "kao_prc_bit.txt"
+    var kaoPrcFrmInitFile = "kao_prc_frm.txt"
+    var kaoOutStreamInitFile = "kao_out_stream.txt"
+    @SuppressLint("SdCardPath")
+    var kaoBackupDir = "/sdcard/Download"
+    var kaoBackupFiles = "KAO_BACKUP.gz"
+    @SuppressLint("SdCardPath")
+    var kaoFiles = "/sdcard/Android/data/com.hana.kizunaaudiocontroller/files/Download/"
+    @SuppressLint("SdCardPath")
+    var uhqaForceFile =
         "/sdcard/Android/data/com.hana.kizunaaudiocontroller/files/Download/uhqa.txt"
-    var hph_force_file =
+    @SuppressLint("SdCardPath")
+    var hphForceFile =
         "/sdcard/Android/data/com.hana.kizunaaudiocontroller/files/Download/hph.txt"
-    var amp_force_file =
+    @SuppressLint("SdCardPath")
+    var ampForceFile =
         "/sdcard/Android/data/com.hana.kizunaaudiocontroller/files/Download/amp.txt"
-    var impedance_force_file =
+    @SuppressLint("SdCardPath")
+    var impedanceForceFile =
         "/sdcard/Android/data/com.hana.kizunaaudiocontroller/files/Download/impedance.txt"
-    var gating_force_file =
+    @SuppressLint("SdCardPath")
+    var gatingForceFile =
         "/sdcard/Android/data/com.hana.kizunaaudiocontroller/files/Download/gating.txt"
-    var kernel_dump = "/sdcard/Android/data/com.hana.kizunaaudiocontroller/files/Download/kdump.txt"
-    var mediaflinger_dump =
+    @SuppressLint("SdCardPath")
+    var kernelDump = "/sdcard/Android/data/com.hana.kizunaaudiocontroller/files/Download/kdump.txt"
+    @SuppressLint("SdCardPath")
+    var mediaflingerDump =
         "/sdcard/Android/data/com.hana.kizunaaudiocontroller/files/Download/afl.txt"
-    var unfiltered_library =
+    @SuppressLint("SdCardPath")
+    var unfilteredLibrary =
         "/sdcard/Android/data/com.hana.kizunaaudiocontroller/files/Download/un_library.txt"
+    @SuppressLint("SdCardPath")
     var library = "/sdcard/Android/data/com.hana.kizunaaudiocontroller/files/Download/library.txt"
-    var audio_client_tl_init =
-        "/sdcard/Android/data/com.hana.kizunaaudiocontroller/files/Download/audio_client_tl_init.txt"
-    var audio_client_bl_init =
-        "/sdcard/Android/data/com.hana.kizunaaudiocontroller/files/Download/audio_client_bl_init.txt"
-    var audio_client_init =
-        "/sdcard/Android/data/com.hana.kizunaaudiocontroller/files/Download/audio_client_init.txt"
-    var kao_audio_init =
-        "/sdcard/Android/data/com.hana.kizunaaudiocontroller/files/Download/kao_audio_init.txt"
-    var kao_aclient =
+    @SuppressLint("SdCardPath")
+    var audioClientTlInit =
+        "/sdcard/Android/data/com.hana.kizunaaudiocontroller/files/Download/audio_client_tlInit.txt"
+    @SuppressLint("SdCardPath")
+    var audioClientBlInit =
+        "/sdcard/Android/data/com.hana.kizunaaudiocontroller/files/Download/audio_client_blInit.txt"
+    @SuppressLint("SdCardPath")
+    var audioClientInit =
+        "/sdcard/Android/data/com.hana.kizunaaudiocontroller/files/Download/audio_clientInit.txt"
+    @SuppressLint("SdCardPath")
+    var kaoAudioInit =
+        "/sdcard/Android/data/com.hana.kizunaaudiocontroller/files/Download/kao_audioInit.txt"
+    @SuppressLint("SdCardPath")
+    var kaoAclient =
         "/sdcard/Android/data/com.hana.kizunaaudiocontroller/files/Download/kao_aclient.txt"
-    var kao_aclient_top_limit =
+    @SuppressLint("SdCardPath")
+    var kaoAclientTopLimit =
         "/sdcard/Android/data/com.hana.kizunaaudiocontroller/files/Download/kao_aclient_tl.txt"
-    var kao_aclient_bottom_limit =
+    @SuppressLint("SdCardPath")
+    var kaoAclientBottomLimit =
         "/sdcard/Android/data/com.hana.kizunaaudiocontroller/files/Download/kao_aclient_bl.txt"
-    var kao_io_init =
+    @SuppressLint("SdCardPath")
+    var kaoIoInit =
         "/sdcard/Android/data/com.hana.kizunaaudiocontroller/files/Download/kao_io.txt"
-    var kao_standby_init =
+    @SuppressLint("SdCardPath")
+    var kaoStandbyInit =
         "/sdcard/Android/data/com.hana.kizunaaudiocontroller/files/Download/kao_standby.txt"
-    var kao_smp_rate_init =
+    @SuppressLint("SdCardPath")
+    var kaoSmpRateInit =
         "/sdcard/Android/data/com.hana.kizunaaudiocontroller/files/Download/kao_smp_rate.txt"
-    var kao_frm_cnt_init =
+    @SuppressLint("SdCardPath")
+    var kaoFrmCntInit =
         "/sdcard/Android/data/com.hana.kizunaaudiocontroller/files/Download/kao_frm_cnt.txt"
-    var kao_frm_bit_init =
+    @SuppressLint("SdCardPath")
+    var kaoFrmBitInit =
         "/sdcard/Android/data/com.hana.kizunaaudiocontroller/files/Download/kao_frm_bit.txt"
-    var kao_chn_cnt_init =
+    @SuppressLint("SdCardPath")
+    var kaoChnCntInit =
         "/sdcard/Android/data/com.hana.kizunaaudiocontroller/files/Download/kao_chn_cnt.txt"
-    var kao_chn_mask_init =
+    @SuppressLint("SdCardPath")
+    var kaoChnMaskInit =
         "/sdcard/Android/data/com.hana.kizunaaudiocontroller/files/Download/kao_chn_mask.txt"
-    var kao_prc_bit_init =
+    @SuppressLint("SdCardPath")
+    var kaoPrcBitInit =
         "/sdcard/Android/data/com.hana.kizunaaudiocontroller/files/Download/kao_prc_bit.txt"
-    var kao_prc_frm_init =
+    @SuppressLint("SdCardPath")
+    var kaoPrcFrmInit =
         "/sdcard/Android/data/com.hana.kizunaaudiocontroller/files/Download/kao_prc_frm.txt"
-    var kao_out_stream_init =
+    @SuppressLint("SdCardPath")
+    var kaoOutStreamInit =
         "/sdcard/Android/data/com.hana.kizunaaudiocontroller/files/Download/kao_out_stream.txt"
 }
